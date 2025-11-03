e8d0df86cd00059551c27fc22165b64ceb1f7213
Geoffrey Chandler <geoffc@gmail.com>
2025-11-03 19:04:38 +0100

    Update AWS SSO Profile to version 0.3.3

--- Formula/aws-sso-profile.rb
+++ Formula/aws-sso-profile.rb
@@ -1,13 +1,14 @@
 class AwsSsoProfile < Formula
   desc "Generate and manage AWS CLI profiles using AWS SSO sessions"
   homepage "https://github.com/advantageous/AWS-SSO-Profile-Manager"
-  url "https://github.com/advantageous/AWS-SSO-Profile-Manager/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "e6058cae35258f32e3b269057654fd25e2358d684b2c0b8cc2957d274cbac0b9"
+  url "https://github.com/advantageous/AWS-SSO-Profile-Manager/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "76a11287a2ce8e6c438c8123062b3bcebe2b465b319e293214951ea5d5ad6aff"
   license "MIT"
 
   depends_on "bash"
   depends_on "jq"
   depends_on "yq"
+  depends_on "awscli"
 
   def install
     bin.install "aws-sso-profile"
@@ -15,14 +16,13 @@ class AwsSsoProfile < Formula
 
   test do
     (testpath/"sessions.yaml").write <<~YAML
-      Advantageous:
-        prefix: adv
+      adv:
         start_url: https://advantageous.awsapps.com/start
         sso_region: eu-central-1
     YAML
 
     assert_match "aws-sso-profile", shell_output("#{bin}/aws-sso-profile --version")
-    assert_match "Advantageous",
+    assert_match "adv",
       shell_output("#{bin}/aws-sso-profile list --import-file #{testpath}/sessions.yaml --dry-run")
 
     system "#{bin}/aws-sso-profile", "generate",
